b9e16d01d3ca0927101222c2325ad13a83df8d32
Sittichaikim <sittichaikim27@gmail.com>
2024-03-14 02:54:39 +0700

    fix vite

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,14 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css', 
+                'resources/js/app.js',
+                'resources/js/main.js',
+                'resources/js/plugins.js',
+                'resources/js/bootstrap.js',
+                'resources/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js',
+        ],
             refresh: true,
         }),
     ],
