32bf48c6fdffd2a185b296fd0b79e33f71596667
Sittichaikim <sittichaikim27@gmail.com>
2024-03-14 01:32:39 +0700

    fix user

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,10 +9,20 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use Filament\Panel;
+
+
 
 class User extends Authenticatable implements FilamentUser
 {
+
+    
+    public function canAccessFilament(): bool
+    {
+        return str_ends_with($this->email, '@yourdomain.com') && $this->hasVerifiedEmail();
+    }
+
+
+
     use HasApiTokens, HasFactory, Notifiable;
 
     /**
@@ -21,10 +31,6 @@ class User extends Authenticatable implements FilamentUser
      * @var array<int, string>
      */
 
-     public function canAccessPanel(Panel $panel): bool
-     {
-         return str_ends_with($this->email, '@yourdomain.com') && $this->hasVerifiedEmail();
-     }
 
     protected $fillable = [
         'name',
