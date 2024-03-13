fabdb2ac29bec68bd23ed1a284778a3e97df7dae
Sittichaikim <sittichaikim27@gmail.com>
2024-03-14 02:50:54 +0700

    fix random

--- resources/views/random.blade.php
+++ resources/views/random.blade.php
@@ -17,112 +17,113 @@
 
     </div> --}}
 
-<div class="d-none d-lg-block ">
-    <div class="full-screen-portfolio bg-game d-flex justify-content-center align-items-center"
-        style="padding-right: 200px; padding-left: 200px;  height: 100vh; " id="portfolio">
-        <div class="container-fluid  ">
-            <div class="modal-dialog modal-lg "  role="document">
+    <div class="d-none d-lg-block ">
+        <div class="full-screen-portfolio bg-game d-flex justify-content-center align-items-center"
+            style="padding-right: 200px; padding-left: 200px;  height: 100vh; " id="portfolio">
+            <div class="container-fluid  ">
+                <div class="modal-dialog modal-lg " role="document">
 
                     <div class="p-3 py-5" id="randomImageContainer">
                     </div>
-                    
+
+                </div>
+                <div>
+
+                    <div class="mb-5">
+                        <!-- แถวของ 5 ภาพ -->
+                        <div class="row d-flex justify-content-center " id="photohid">
+
+                            @php $count = 0; @endphp
+                            @foreach ($Random as $Randoms)
+                                @php $count++; @endphp
+                                @if ($count <= 5)
+                                    <div class="col-md-2 col-sm-3 portfolio-item mt-5 px-3">
+                                        <a href="{{ asset('storage/' . $Randoms->Random_img) }}" data-fancybox="gallery">
+                                            <div class="thumb">
+                                                <div class="hover-effect">
+                                                    <div class="hover-content">
+                                                        <p>{{ $Randoms->Random_name }}</p>
+                                                    </div>
+                                                </div>
+                                                <div class="fix-img-ran">
+                                                    <img src="{{ asset('storage/' . $Randoms->Random_img) }}"
+                                                        style="border-radius: 10px;" class="not-selected">
+                                                </div>
+                                            </div>
+                                        </a>
+                                    </div>
+                                @endif
+                            @endforeach
+                        </div>
+
+                        <!-- แถวของ 5 ภาพ -->
+                        <div class="row d-flex justify-content-center " id="photohid">
+                            @php $count = 0; @endphp
+                            @foreach ($Random as $Randoms)
+                                @php $count++; @endphp
+                                @if ($count > 5 && $count <= 9)
+                                    <div class="col-md-2 col-sm-3 portfolio-item mt-5 px-3">
+                                        <a href="{{ asset('storage/' . $Randoms->Random_img) }}" data-fancybox="gallery">
+                                            <div class="thumb">
+                                                <div class="hover-effect">
+                                                    <div class="hover-content">
+                                                        <p>{{ $Randoms->Random_name }}</p>
+                                                    </div>
+                                                </div>
+                                                <div class="fix-img-ran">
+                                                    <img src="{{ asset('storage/' . $Randoms->Random_img) }}"
+                                                        style="border-radius: 10px;" class="not-selected">
+                                                </div>
+                                            </div>
+                                        </a>
+                                    </div>
+                                @endif
+                            @endforeach
+                        </div>
+
+                        <!-- แถวของ 5 ภาพ -->
+                        <div class="row d-flex justify-content-center " id="photohid">
+                            @php $count = 0; @endphp
+                            @foreach ($Random as $Randoms)
+                                @php $count++; @endphp
+                                @if ($count > 9 && $count <= 12)
+                                    <div class="col-md-2 col-sm-3 portfolio-item mt-5 px-3">
+                                        <a href="{{ asset('storage/' . $Randoms->Random_img) }}" data-fancybox="gallery">
+                                            <div class="thumb">
+                                                <div class="hover-effect">
+                                                    <div class="hover-content">
+                                                        <p>{{ $Randoms->Random_name }}</p>
+                                                    </div>
+                                                </div>
+                                                <div class="fix-img-ran">
+                                                    <img src="{{ asset('storage/' . $Randoms->Random_img) }}"
+                                                        style="border-radius: 10px;" class="not-selected">
+                                                </div>
+                                            </div>
+                                        </a>
+                                    </div>
+                                @endif
+                            @endforeach
+
+
+                        </div>
+
+                    </div>
+
+
+                    <div class="d-flex justify-content-center ">
+                        <button class="btn btn-primary" type="button" id="randomButton">Random</button>
+                    </div>
+
+                </div>
             </div>
-            <div>
-            
-                <!-- แถวของ 5 ภาพ -->
-                <div class="row d-flex justify-content-center " id="photohid">
-
-                    @php $count = 0; @endphp
-                    @foreach ($Random as $Randoms)
-                        @php $count++; @endphp
-                        @if ($count <= 5)
-                            <div class="col-md-2 col-sm-3 portfolio-item mt-5 px-3">
-                                <a href="{{ asset('storage/' . $Randoms->Random_img) }}" data-fancybox="gallery">
-                                    <div class="thumb">
-                                        <div class="hover-effect">
-                                            <div class="hover-content">
-                                                <p>{{ $Randoms->Random_name }}</p>
-                                            </div>
-                                        </div>
-                                        <div class="fix-img-ran">
-                                            <img src="{{ asset('storage/' . $Randoms->Random_img) }}"
-                                                style="border-radius: 10px;" class="not-selected">
-                                        </div>
-                                    </div>
-                                </a>
-                            </div>
-                        @endif
-                    @endforeach
-                </div>
-
-                <!-- แถวของ 5 ภาพ -->
-                <div class="row d-flex justify-content-center " id="photohid">
-                    @php $count = 0; @endphp
-                    @foreach ($Random as $Randoms)
-                        @php $count++; @endphp
-                        @if ($count > 5 && $count <= 9)
-                            <div class="col-md-2 col-sm-3 portfolio-item mt-5 px-3">
-                                <a href="{{ asset('storage/' . $Randoms->Random_img) }}" data-fancybox="gallery">
-                                    <div class="thumb">
-                                        <div class="hover-effect">
-                                            <div class="hover-content">
-                                                <p>{{ $Randoms->Random_name }}</p>
-                                            </div>
-                                        </div>
-                                        <div class="fix-img-ran">
-                                            <img src="{{ asset('storage/' . $Randoms->Random_img) }}"
-                                                style="border-radius: 10px;" class="not-selected">
-                                        </div>
-                                    </div>
-                                </a>
-                            </div>
-                        @endif
-                    @endforeach
-                </div>
-
-                <!-- แถวของ 5 ภาพ -->
-                <div class="row d-flex justify-content-center " id="photohid">
-                    @php $count = 0; @endphp
-                    @foreach ($Random as $Randoms)
-                        @php $count++; @endphp
-                        @if ($count > 9 && $count <= 12)
-                            <div class="col-md-2 col-sm-3 portfolio-item mt-5 mb-5 px-3">
-                                <a href="{{ asset('storage/' . $Randoms->Random_img) }}" data-fancybox="gallery">
-                                    <div class="thumb">
-                                        <div class="hover-effect">
-                                            <div class="hover-content">
-                                                <p>{{ $Randoms->Random_name }}</p>
-                                            </div>
-                                        </div>
-                                        <div class="fix-img-ran">
-                                            <img src="{{ asset('storage/' . $Randoms->Random_img) }}"
-                                                style="border-radius: 10px;" class="not-selected">
-                                        </div>
-                                    </div>
-                                </a>
-                            </div>
-                        @endif
-                    @endforeach
 
 
-                </div>
 
 
-            
 
-                <div class="d-flex justify-content-center ">
-                    <button class="btn btn-primary" type="button" id="randomButton">Random</button>
-                </div>
-
-            </div>
         </div>
-
-
-
-
-
     </div>
-</div>
 
 
 
@@ -130,7 +131,7 @@
     <div class="modal fade" id="resultModal" tabindex="-1" role="dialog" aria-labelledby="resultModalLabel"
         aria-hidden="true">
         <div class="modal-dialog modal-lg" role="document">
-            
+
         </div>
     </div>
 
